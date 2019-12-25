353a00073ac2acef14652fe68823c0e2a2d7918b
Rwarlock <bhanukrsingh@gmail.com>
2019-12-26 00:58:26 +0530

    Working with loops

--- index.php
+++ index.php
@@ -2,15 +2,35 @@
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
+
+        <?php if( have_posts() ): while (have_posts() ) : the_post(); ?>
+
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <header class="entry-header">
-                <h1>index.php</h1>
+            <?php the_title( '<h1>','<h1>' )  ?>
             </header>
 
             <div class="entry-content">
-            lorem
+                <?php the_content(); ?>
             </div>
         </article>
+        <?php endwhile; else :  ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <h1><?php esc_html_e('404', 'wphierarchy');?>
+
+                </h1>
+
+
+            </header>
+
+            <div class="entry-content">
+                <?php esc_html_e('sorry no content found', 'wphierarchy');?>
+            </div>
+        </article>
+
+        <?php endif; ?>
+
     </main>
 </div>
 
