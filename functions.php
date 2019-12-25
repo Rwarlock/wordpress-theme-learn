d0de59862856aaed193c52b116bc84f0dad01a17
Rwarlock <bhanukrsingh@gmail.com>
2019-12-25 22:59:50 +0530

    initial push

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+
+// Add theme support
+
+add_theme_support('title-tag');
+add_theme_support('post_thumbnails');
+add_theme_support('post-format',['aside','gallery','link','image','quote','status','video','audio','chat']);
+add_theme_support('html5');
+add_theme_support('automatic-feed-links');
+add_theme_support('custom-background');
+add_theme_support('custom-header');
+add_theme_support('custom-logo');
+add_theme_support('customize-selective-refresh-widgets');
+add_theme_support('starter-content');
+
+
+
+// Load in css
+
+function wphierarchy_enqueue_styles() {
+
+    wp_enqueue_style('main-css', get_stylesheet_uri() , [], time(), 'all');
+}
+add_action('wp_enqueue_scripts', 'wphierarchy_enqueue_styles' );
+
+
+
+//Register Menu Locations
+
+register_nav_menus([
+    'main-menu' => esc_html__('Main Menu', 'wpheirarchy')
+]);
+
+?>
